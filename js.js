96f85de00db743389570f651f533805c49b58350
Yura-Andriets <Yurii_2011@ukr.net>
2020-02-05 13:21:15 +0200

    add output result in one line

--- js.js
+++ js.js
@@ -7,66 +7,83 @@ function payProduct(){
     let product3  = document.getElementById("product3").value;
     let payProduct  = document.getElementById("payProduct").value;
 
-// Вывод максимального числа
+// Вывод максимальной цены
     let maxNumber = Math.max(+product1, +product2, +product3);
-    console.log("Максимальное число: " + +maxNumber);
 
-// Вывод минимального числа
+
+// Вывод минимальной цены
     let minNumber = Math.min(+product1, +product2, +product3);
-    console.log("Минимальное число: " + +minNumber);
+
 
 // Вывод суммы всех товаров
     let sum = +product1 + +product2 + + product3;
-    console.log("Сумма всех товаров: " + +sum);
+
 
 // Вывод суммы округленная в МЕНЬШУЮ сторону
     let sumMin = Math.floor(+product1) + Math.floor(+product2) + Math.floor(+product3);
-    console.log("Сумма округленная в МЕНЬШУЮ сторону: " + +sumMin);
+
 
 //Вывод суммы товаров округленную до сотен
-    let q = (Math.round(+sumMin/100)*100);
-    console.log("Сумма товаров округленную до сотен: " + +q);
+    let amount = (Math.round(+sumMin/100)*100);
+
 
 //Вывод булевого значение
-    let boole = sumMin % 2 === 0 ? console.log("сумма всех товаров - четное!") : console.log("сумма всех товаров - нечетное!");
+    let boole = sumMin % 2 === 0 ? "ДА" : "НЕТ!";
 
 
 //Вывод суммы сдачи
     let rest = +payProduct - +sum;
-    console.log("Сумма сдачи: " + +rest);
+
 
 //Вывод среднего значение цен всех товаров
     let middel = (sum/3).toFixed(2);
-    console.log("Среднее значение цен всех товаров: " + +middel);
+
 
 //Случайная скидка
     let discount = Math.random();
-    console.log("Случайная скидка: " + Math.round(+discount*10) + " %");
+    discount = Math.round(+discount*10);
+
 
 //Сумма к оплате со скидкой округленная до 2 знака после запятой
-    let sale = (sum - (sum/100)*(+discount*10)).toFixed(2);
-    console.log("Сумма к оплате со скидкой округленная до 2 знака после запятой: " + +sale);
+    let sale = (sum - (sum/100)*(+discount)).toFixed(2);
 
 
 
-    console.log("Adwance");
+
+    //"Adwance";
 
 //Округоенная скидка    
-    let discountProduct = Math.round((sum/100)*(+discount*10));
+    let discountProduct = Math.round((sum/100)*(+discount));
 
 
-//Оплачено со скидкой
+//Сумма к оплате с учетом скидки
     let saleProduct = Math.round(sum - +discountProduct);
-    console.log("Оплачено со скидкой: " + +saleProduct);
+
 
 //Себестоимость
     let cost = Math.round(+sum)/2;
-    console.log("Себестоимость: " + +cost);
+
 
 //Чистая прибыль
     let profit = +cost - +discountProduct;
-    console.log("Чистая прибыль: " + +profit);
 
-   }
+
+    console.log(`
+    Максимальная цена ${maxNumber}
+    Минимальная цена ${minNumber}
+    Сумма всех товаров: ${sum}
+    Сумма округленная в МЕНЬШУЮ сторону: ${sumMin}
+    Сумма товаров округленную до сотен: ${amount}
+    Цена всех товаров четное! - ${boole}
+    Сумма сдачи: ${rest}
+    Среднее значение цен всех товаров: ${middel}
+    Случайная скидка: ${discount} %
+    Сумма к оплате со скидкой округленная до 2 знака после запятой: ${sale}
+    Сумма к оплате с учетом скидки ${saleProduct}
+    Себестоимость: ${cost}
+    Чистая прибыль: ${profit}
+   `)
+
+}
 
 document.getElementById("price").onclick = payProduct;
